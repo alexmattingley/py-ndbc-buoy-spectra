0a023fba2de7b6930f21a487d5083c0a69377ea1
Alex Mattingley <alexmattingely@gmail.com>
2016-04-24 12:58:44 -0700

    moving everyting over

--- routes/buoy.js
+++ routes/buoy.js
@@ -1,13 +1,62 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
-var set_flags = require('./set_flags');
+var exec = require('child_process').exec;
+
+var pyArgs = {
+  // make arguments that take no parameters (ie, --json) true or false
+  "buoy": '46232',
+  "datasource": 'http',
+  "json": true,
+  "datatype": "spectra",
+  "units": 'ft'
+};
+
+pyArgs.datatype = '9band';
+
+function flagGen(args) {
+  var flags = '';
+  for (var a in args) {
+    if (args.hasOwnProperty(a)) {
+      if (typeof(pyArgs[a]) == 'string'){
+        flags += " --" + a + ' ' + pyArgs[a];
+      }
+      else {
+        if (pyArgs[a] == true)
+          flags += ' --' + a;
+      }
+    }
+  }
+  return flags;
+}
+
+function check_script_complete(output){
+  //check if output exists
+  //wait a second
+  //check again
+  //call function as middlewear in child variable?
+}
+
+function run_script(){
+  var buoyData = ''
+  var execstr = 'python ' + path.join('./', 'ndbc.py') + flagGen(pyArgs);
+  var child = exec(execstr, function(error, stdout, stderr) {
+    console.log('working');
+    if (error) {
+      console.log(stderr)
+    }
+    else {
+      buoyData= JSON.parse(stdout);
+    }
+  });
+  return buoyData;
+}
+
 
 
 /* GET buoy page. */
 router.get('/', function(req, res, next) {
   var buoy_id = req.query.buoy_id;
-  var buoyData = set_flags.run_script();
 	res.send(set_flags.run_script());
 });
 
